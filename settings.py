41e9160865429f5456dd1b1bc45b7f067d3fa4ce
Deep Purohit <DeepPurohit@users.noreply.github.com>
2018-01-07 05:36:14 +0530

    Secret key has been removed

--- settings.py
+++ settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '+f)-!^p^9(5_9+d-(@clav8c!!&@yfn_xp-5(u_uffgyo_dk+k'
+SECRET_KEY = 'User Your Own Secret Key'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
